38835768b1008e0956b8d10e9161f782b0ae6bae
Danbi <danbik77@gmail.com>
2024-01-08 05:56:33 -0800

    wedd

--- src/main/java/com/example/demo/practice/dddddddddddddddddddddddddddd.java
+++ src/main/java/com/example/demo/practice/dddddddddddddddddddddddddddd.java
@@ -2,7 +2,7 @@ package com.example.demo.practice;
 
 public class dddddddddddddddddddddddddddd {
 	
-	public String test;
+	public String test;wewdw
 	private void syso() {
 		// TODO Auto-generated method stub
 
